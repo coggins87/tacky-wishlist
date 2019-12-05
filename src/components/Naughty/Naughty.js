1497b38a2759b43f60d2ebe1e50e9ed58aa2a99f
Colleen <colleenhiggins87@gmail.com>
2019-12-04 17:43:48 -0800

    added annoying music to naughty and nice

--- src/components/Naughty/Naughty.js
+++ src/components/Naughty/Naughty.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import LetterToSanta from '../LetterToSanta/LetterToSanta'
 import './Naughty.css'
-
+import nuttin from '../../assets/Imgettingnuttin.mp3'
 export default class Naughty extends React.Component{
  componentDidMount(){
    window.scrollTo(0, 0)
@@ -10,6 +10,8 @@ export default class Naughty extends React.Component{
   render (){ 
   return (
     <div className="naughty">
+              <audio autoPlay loop src={nuttin}></audio>
+
     <LetterToSanta />
     </div>
   )
--- src/components/Nice/Nice.js
+++ src/components/Nice/Nice.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import LetterToSanta from '../LetterToSanta/LetterToSanta'
 import './Nice.css'
+import chipmunks from '../../assets/chipmunks.mp3'
+
 export default class Nice extends React.Component{
   componentDidMount(){
     window.scrollTo(0, 0)
@@ -8,6 +10,7 @@ export default class Nice extends React.Component{
   render (){
   return (
     <div className="nice">
+      <audio autoPlay loop src={chipmunks} />
     <LetterToSanta />
     </div>
   )
