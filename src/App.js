dda7e3fdbc54bac436009cd240a443c20b02d5d4
Colleen <colleenhiggins87@gmail.com>
2019-12-04 16:58:49 -0800

    routes in place

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
-
-import LetterToSanta from './components/LetterToSanta/LetterToSanta';
-
+import {Router, Route, Switch} from 'react-router-dom'
+import Naughty from './components/Naughty/Naughty'
+import Nice from './components/Nice/Nice'
 import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
-
-     <LandingPage />
-     <LetterToSanta />
-
+  <Switch>
+  <Route path='/' exact component={LandingPage}/>
+  <Route path='/naughty' component={Naughty}/>
+  <Route path='/nice' component={Nice}/>
+  </Switch>
 
     </div>
   );
